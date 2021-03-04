ff6f997a927fca6477ce19ef7cbf5c3b0d8092e7
Sushant S Sundikar <sushant.sundikar@gmail.com>
2021-03-05 03:40:39 +0530

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,11 +1,3 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
-  file { '/root/README':
-    ensure => file,
-    owner   => 'root',
-  }
+ 
  }
